b6bf098e999ddd8081c197e0aded85162e560328
jsarzi <justine.sarzi@epitech.eu>
2022-11-14 14:27:47 +0100

    routes done

--- routes/discussions.routes.js
+++ routes/discussions.routes.js
@@ -6,24 +6,39 @@ const Message = require("../models/Message.model");
 const { isLoggedInFunction } = require("../middlewares/middlewares");
 
 router.get("/", isLoggedInFunction, async (req, res) => {
-  const currentUserConvo = await User.findOne({ username: req.session.currentUser.username });
+  const currentUserConvo = await User.findOne({
+    username: req.session.currentUser.username,
+  });
   console.log(currentUserConvo.discussions);
-  res.render("discussions", { currentUserConvo });
+  res.render("discussions/discussions", { currentUserConvo });
 });
 
 router.get("/:id", async (req, res) => {
-  const currentConversation = await Discussion.findById(req.params.id).populate("messages");
+  const currentConversation = await Discussion.findById(req.params.id).populate(
+    "messages"
+  );
   const { messages } = currentConversation;
   console.log(`sending ${messages}`);
-  res.render("conversation", { messages, id: currentConversation._id, scriptName: "conversation" });
+  res.render("discussions/single-discussion", {
+    messages,
+    id: currentConversation._id,
+    scriptName: "conversation",
+  });
 });
 
 router.post("/:id", async (req, res) => {
   console.log(req.body["new-message-value"]);
-  const newMessage = await Message.create({ sender: req.session.currentUser.username, content: req.body["new-message-value"] });
+  const newMessage = await Message.create({
+    sender: req.session.currentUser.username,
+    content: req.body["new-message-value"],
+  });
   console.log(newMessage._id);
   console.log(req.params.id);
-  console.log(await Discussion.findByIdAndUpdate(req.params.id, { $push: { messages: newMessage._id } }));
+  console.log(
+    await Discussion.findByIdAndUpdate(req.params.id, {
+      $push: { messages: newMessage._id },
+    })
+  );
   res.redirect(req.params.id);
 });
 
--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,9 +1,25 @@
 const express = require("express");
 const router = express.Router();
 
+//HOME
 router.get("/", (req, res) => res.render("home"));
-router.use("/users", require("./user.routes"));
-router.use("/auth", require("./auth.routes"));
+
+// INTERESTS
+router.get("/interests", (req, res) => res.render("interests"));
+
+// PROFILE
+router.get("/profile", (req, res) => res.render("profile"));
+
+// ACTIVITIES
+router.use("/activities", require("./activities.routes"));
+
+// REQUESTS
+router.use("/requests", require("./requests.routes"));
+
+// DISCUSSION
 router.use("/discussions", require("./discussions.routes"));
+
+//AUTH
+router.use("/auth", require("./auth.routes"));
 
 module.exports = router;
