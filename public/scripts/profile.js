f29bdbd9166ad554c98ac1db99df5a69dc4809d7
jsarzi <justine.sarzi@epitech.eu>
2022-11-16 12:46:53 +0100

    profile in process

--- public/scripts/profile.js
+++ public/scripts/profile.js
@@ -1,6 +1,19 @@
 const updateImage = document.querySelector(".updateImage");
 const file = document.querySelector("#profilePicture");
 const deleate = document.querySelector(".deleate");
+const image = document.querySelector('#image');
+const edit = document.querySelector('#edit');
 updateImage.addEventListener("click", () => {
   file.click();
 });
+
+file.onchange = evt => {
+  const [imgFile] = file.files
+  if (imgFile) {
+    image.src = URL.createObjectURL(imgFile)
+  }
+}
+
+edit.addEventListener('click',()=>{
+  window.location.assign('/interests')
+})
