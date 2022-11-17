3f3543d671948161927aa3151c76b1ddbd856715
Sam Sinz <sam@sinz.fr>
2022-11-17 12:21:54 +0100

    eventlisteners single request

--- public/scripts/requests/single-request.js
+++ public/scripts/requests/single-request.js
@@ -1,4 +1,17 @@
  document.addEventListener("DOMContentLoaded", function () {
-    const description = document.querySelector('#description')
+    const description = document.querySelector('.description')
     description.textContent = description.textContent.slice(0,50) + '...';
+    const matchId = document.querySelector('#hidden-id').textContent
+
+    console.log('hihi')
+
+    document.querySelector('#decline').addEventListener("click", () => {
+      console.log('decline')
+      window.location = `${matchId}/decline`
+    })
+    document.querySelector('#approve').addEventListener("click", () => {
+      console.log('approve')
+      window.location = `${matchId}/approve`
+    })
+
   });
