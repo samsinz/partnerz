c75cc6c43c5f230ea1a790da2ac2e54e239d391c
olxmpe <olympe.lespagnon@hotmail.fr>
2022-11-17 10:49:37 +0100

    single request route + hbs + css

---
+++ public/scripts/requests/single-request.js
@@ -0,0 +1,4 @@
+ document.addEventListener("DOMContentLoaded", function () {
+    const description = document.querySelector('#description')
+    description.textContent = description.textContent.slice(0,50) + '...';
+  });
