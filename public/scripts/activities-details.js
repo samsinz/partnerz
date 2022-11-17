49dcb024c2659796164511c6576787e35ad2f6fc
olxmpe <olympe.lespagnon@hotmail.fr>
2022-11-17 12:42:11 +0100

    new activities + price displayed on single activities

--- public/scripts/activities-details.js
+++ public/scripts/activities-details.js
@@ -1,11 +1,33 @@
 
 document.addEventListener("DOMContentLoaded", function () {
 
+const price = document.querySelector("#price").textContent
+    const light = document.querySelector("#light-span") 
+    const bold = document.querySelector("#bold-span") 
+
+    console.log(price)
+    if (price === "0") {
+        light.textContent = "€€€€"
+    } else if (price === "1") {
+        bold.textContent = "€" 
+        light.textContent = "€€€"       
+    } else if (price === "2") {  
+        bold.textContent = "€€" 
+        light.textContent = "€€"      
+    } else if (price === "3") {
+        bold.textContent = "€€€" 
+        light.textContent = "€" 
+    } else {
+        bold.textContent = "€€€€" 
+    }
+
+
+
 const partnersButton = document.querySelector("#partners");
 
-console.log(partnersButton)
 partnersButton.addEventListener('click', ()=>{
     const activityId = document.querySelector('#activity-id').textContent
     window.location = `${activityId}/partners`
+
 })
 })
