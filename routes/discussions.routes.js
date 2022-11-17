c64c236886f780a2e56cb1d7e064ca54c6a46111
jsarzi <justine.sarzi@epitech.eu>
2022-11-17 12:21:48 +0100

    conversation

--- routes/discussions.routes.js
+++ routes/discussions.routes.js
@@ -2,15 +2,38 @@ const router = require("express").Router();
 const User = require("../models/User.model");
 const Discussion = require("../models/Discussion.model");
 const Message = require("../models/Message.model");
+const Match = require("../models/Match.model");
 
 const { isLoggedInFunction } = require("../middlewares/middlewares");
 
 router.get("/", isLoggedInFunction, async (req, res) => {
-  const currentUserConvo = await User.findOne({
+  const allMatchesId = req.session.currentUser.matches;
+  const currentUserConvo = await User.find({
     username: req.session.currentUser.username,
   });
-  console.log(currentUserConvo.discussions);
-  res.render("discussions/discussions", { currentUserConvo });
+
+
+  
+  const allMatches = await Match.find({_id: allMatchesId, status:'Pending'})
+    .populate({path: 'discussion', populate: {path: 'messages', model: 'Message'}})
+    .populate('receiver')
+    .populate('sender')
+
+    
+    console.log(currentUserConvo);
+    
+  // const lastMessage = allMatches.discussion.messages[0]
+ 
+    res.render("discussions/discussions", { allMatches, scriptName: 'discussions/discussions', styleName:'discussions/discussions'});
+  
+
+
+
+    
+
+
+
+  
 });
 
 router.get("/:id", async (req, res) => {
@@ -18,7 +41,7 @@ router.get("/:id", async (req, res) => {
     "messages"
   );
   const { messages } = currentConversation;
-  console.log(`sending ${messages}`);
+  // console.log(`sending ${messages}`);
   res.render("discussions/single-discussion", {
     messages,
     id: currentConversation._id,
@@ -27,18 +50,18 @@ router.get("/:id", async (req, res) => {
 });
 
 router.post("/:id", async (req, res) => {
-  console.log(req.body["new-message-value"]);
+  // console.log(req.body["new-message-value"]);
   const newMessage = await Message.create({
     sender: req.session.currentUser.username,
     content: req.body["new-message-value"],
   });
-  console.log(newMessage._id);
-  console.log(req.params.id);
-  console.log(
-    await Discussion.findByIdAndUpdate(req.params.id, {
-      $push: { messages: newMessage._id },
-    })
-  );
+  // console.log(newMessage._id);
+  // console.log(req.params.id);
+  // console.log(
+  //   await Discussion.findByIdAndUpdate(req.params.id, {
+  //     $push: { messages: newMessage._id },
+  //   })
+  // );
   res.redirect(req.params.id);
 });
 
